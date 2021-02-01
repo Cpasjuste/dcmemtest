ee169292d6d690a1e4ab962ee7513783087ad70f
cpasjuste <cpasjuste@gmail.com>
2021-02-01 10:30:50 +0100

    dc: fix kos init

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,49 @@
 //
 
 #include "cross2d/c2d.h"
+#include "main.h"
 
 using namespace c2d;
 
-int main(int argc, char *argv[]) {
+MemTest::MemTest(const Vector2f &size) : C2DRenderer(size) {
+    setClearColor(Color::Gray);
+    getFont()->setFilter(Texture::Filter::Point);
+    getFont()->setOffset({0, 5});
+
+    auto *border = new C2DRectangle({2, 2, getSize().x - 4, getSize().y - 4});
+    border->setFillColor(Color::Transparent);
+    border->setOutlineColor(Color::Yellow);
+    border->setOutlineThickness(2);
+    add(border);
+}
+
+bool MemTest::onInput(c2d::Input::Player *players) {
+
+    unsigned int keys = players[0].keys;
+
+    if (keys & EV_QUIT) {
+        quit = true;
+    }
+
+    return C2DRenderer::onInput(players);
+}
+
+void MemTest::onUpdate() {
+    C2DRenderer::onUpdate();
+}
+
+int main() {
 
     // create main renderer
-    auto *renderer = new C2DRenderer(Vector2f(640, 480));
-    renderer->setClearColor(Color::Gray);
+    auto *memTest = new MemTest({C2D_SCREEN_WIDTH, C2D_SCREEN_HEIGHT});
+    printf("renderer\n");
 
-    while (true) {
-
-        // stop if any key is pressed
-        if (renderer->getInput()->getKeys() != 0) {
-            break;
-        }
-
-        // renderer everything
-        renderer->flip();
+    while (!memTest->quit) {
+        memTest->flip();
     }
 
     // will delete child's (textures, shapes, text..)
-    delete (renderer);
+    delete (memTest);
 
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -5,5 +5,18 @@
 #ifndef DCMEMTEST_MAIN_H
 #define DCMEMTEST_MAIN_H
 
+class MemTest : public c2d::C2DRenderer {
+
+public:
+    explicit MemTest(const c2d::Vector2f &size);
+
+    bool quit = false;
+
+private:
+
+    bool onInput(c2d::Input::Player *players) override;
+
+    void onUpdate() override;
+};
 
 #endif //DCMEMTEST_MAIN_H
